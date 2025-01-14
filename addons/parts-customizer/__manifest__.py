2ef8cb15863ecd9a1bfd8bfb45cfffbf7d18977b
samini <soufianeamini2002@gmail.com>
2025-01-14 18:39:59 +0100

    setup module

---
+++ addons/parts-customizer/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Parts Customizer',
+    'version': '1.2',
+    'description': "",
+    'depends': [
+        'base_setup',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
